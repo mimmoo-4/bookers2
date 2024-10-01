d6d8b0ebd58e8ef6a1e3090e0f8976426c815283
mimmoo-4 <175538898+mimmoo-4@users.noreply.github.com>
2024-10-01 12:30:02 +0000

    userのshowページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,16 @@
 class BooksController < ApplicationController
 
-  def new
-    @book = Book.new
-  end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to books_path
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
     else
-      render :new
+      @user = current_user
+      @books = Book.all
+      render :index
     end
   end
 
@@ -22,6 +22,24 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @book_new = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    is_matching_login_user
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    is_matching_login_user
+    if @book.update(book_params)
+      flash[:notice] = "You can updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -35,6 +53,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    user = User.find(@book.user_id)
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
-  get 'homes/about' => 'homes#about', as: 'about'
-  resources :books
+  get 'home/about' => "homes#about"
   resources :users
+  resources :books
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
